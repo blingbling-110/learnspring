a82a187de992a416dc1b451f7dcfc88752c1616e
blingbling-110 <523497359@qq.com>
2019-08-12 15:46:27 +0800

    P147

--- src/test/java/com/qzj/learn/TestAop.java
+++ src/test/java/com/qzj/learn/TestAop.java
@@ -8,15 +8,29 @@ public class TestAop {
     public static void main(String[] args) {
         ApplicationContext actx = new FileSystemXmlApplicationContext(
                 "exception_config.xml");
-        //动态代理
+        try {
+            //自动动态代理
 //        LogicInterface li = (LogicInterface) actx.getBean("logic1");
 //        li.doInsert("新增");
 //        li.doUpdate("修改");
 //        li.doDelete("删除");
-        //CGLIB代理
-        Logic2 li2 = (Logic2) actx.getBean("logic2");
-        li2.doInsert("新增");
-        li2.doUpdate("修改");
-        li2.doDelete("删除");
+            //自动CGLIB代理
+//        Logic2 l = (Logic2) actx.getBean("logic2");
+//        l.doInsert("新增");
+//        l.doUpdate("修改");
+//        l.doDelete("删除");
+            //手动动态代理
+//        LogicInterface li = (LogicInterface) actx.getBean("logic1Proxy");
+//        li.doInsert("新增");
+//        li.doUpdate("修改");
+//        li.doDelete("删除");
+            //手动CGLIB代理
+            Logic2 l = (Logic2) actx.getBean("logic2Proxy");
+            l.doInsert("新增");
+            l.doUpdate("修改");
+            l.doDelete("删除");
+        }catch (Exception e) {
+
+        }
     }
 }
