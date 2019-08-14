b44efebe5e71128ed5c2bc0a45db01fe48c8ec94
blingbling-110 <523497359@qq.com>
2019-08-14 12:45:19 +0800

    P158

--- src/main/java/com/qzj/learn/HelloDAO.java
+++ src/main/java/com/qzj/learn/HelloDAO.java
@@ -1,8 +1,12 @@
 package com.qzj.learn;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
@@ -19,16 +23,51 @@ public class HelloDAO {
         this.transactionManager = transactionManager;
     }
 
-    //该方法进行事务处理
+    //进行编程式事务处理
     public int create(String msg) {
         TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
         Object result = transactionTemplate.execute(new TransactionCallback() {
             @Override
             public Object doInTransaction(TransactionStatus transactionStatus) {
                 //执行新增的操作，向数据库新增一笔记录
-                return null;
+                JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+                int resultObject = jdbcTemplate.update(
+                        "insert into hello values(1, 'gf', 'HelloWord')");
+                return resultObject;
             }
         });
-        return 0;
+        return (int) result;
     }
+
+    //自己进行回滚、提交
+//    public int create(String msg) {
+//        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+//        TransactionStatus status = transactionManager.getTransaction(def);
+//        int result = 0;
+//        try {
+//            //使用JdbcTemplate往数据库里新增数据
+//            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+//            result = jdbcTemplate.update(
+//                    "insert into hello values(1, 'gf', 'HelloWord')");
+//        } catch (DataAccessException ex) {
+//            //也可以执行status.setRollbackOnly();
+//            transactionManager.rollback(status);
+//            throw ex;
+//        }finally {
+//            transactionManager.commit(status);
+//        }
+//        return result;
+//    }
+
+    //利用TransactionCallback的另一个实现类TransactionCallbackWithoutResult
+//    public void create(String msg) {
+//        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+//        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+//            @Override
+//            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+//                JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+//                jdbcTemplate.update("insert into hello values(1, 'gf', 'HelloWord')");
+//            }
+//        });
+//    }
 }
